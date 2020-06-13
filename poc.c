7fb27d6adde0a5c994da60f862f511070ea0299c
sun <sun012110@gmail.com>
2020-06-13 16:16:12 +0800

    编译poc.so

--- poc.c
+++ poc.c
@@ -7,5 +7,5 @@ build: gcc -shared -o poc.so -fPIC poc.c
 
 static void plugon() __attribute__((constructor));
 void plugon() {
-    system("touch /tmp/go-test/success.txt");
+    system("mkdir /tmp/go-test");
 }
